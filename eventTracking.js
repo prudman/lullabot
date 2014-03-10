15d0d19ad8a627691e9c724cd51ae6e3e64fe102
prudman <patrudman@gmail.com>
2014-03-10 09:10:59 -0400

    Create eventTracking.js

---
+++ eventTracking.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  //Function for Auto Generating Event Tracking
+  var etID = ""; //Event Tracking ID
+
+  $.extend($.expr[':'], { //Creates a custom selector called "track" and applies it to the appropriate elements
+    track: function (obj) {
+      if (obj == null)
+        return false;
+      else {
+        var $obj = $(obj);
+        return $(obj).attr('id') !== undefined && $(obj).attr('id') !== '' && // Has ID
+                  !$(obj).attr('onclick') && !$(obj).attr('onclientclick'); // Does not have "onclick" or "onclientclick" attr.
+      }
+    }
+  });
+
+  $('a:track, input[type="submit"]:track').click(function () {
+    etID = $(this).attr('id').split('_').reverse()[0]; //Creates Array split by '_', reverse it & gets first item
+    _gaq.push(['_trackEvent', 'Events', 'Links', etID]);
+    //window.alert('its working');
+  });
+
+  //Track PDF downloads
+  $("a[href$='pdf']").each(function (index) { //Looks if the href has "pdf" in it.
+    pdfLabel = 'PDF - ' + $(this).attr('href').split('/').reverse()[0]; //Prepends with 'PDF - ' then creates Array split by '/', reverse it & gets first item
+
+    pdfOnClick = "_gaq.push(['_trackEvent', 'Events', 'Links', '" + pdfLabel + "']);";
+    $(this).attr("onClick", pdfOnClick);
+  });
+});
